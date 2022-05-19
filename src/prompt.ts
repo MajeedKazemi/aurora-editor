c35aa9eb2a0665c298f1e342345ea5abcfca63e9
Majeed <majeed@majbook.local>
2022-05-19 12:44:13 -0400

    implemented #3

--- src/prompt.ts
+++ src/prompt.ts
@@ -13,10 +13,33 @@ export class Prompt {
         this.container = document.createElement("div");
         this.container.classList.add("prompt-container");
 
+        const form = document.createElement("form");
+        form.classList.add("prompt-form");
+
+        const input = document.createElement("input");
+        input.classList.add("prompt-input");
+        input.setAttribute(
+            "placeholder",
+            "Generate code based on your description"
+        );
+
+        const button = document.createElement("button");
+        button.classList.add("prompt-button");
+        button.innerText = "Generate";
+
+        form.appendChild(input);
+        form.appendChild(button);
+
+        this.container.appendChild(form);
         document.body.appendChild(this.container);
 
         document.addEventListener("click", (e) => {
-            if (e.target !== this.container) {
+            if (
+                e.x < this.container.offsetLeft ||
+                e.x > this.container.offsetLeft + this.container.offsetWidth ||
+                e.y < this.container.offsetTop ||
+                e.y > this.container.offsetTop + this.container.offsetHeight
+            ) {
                 this.close();
             }
         });
